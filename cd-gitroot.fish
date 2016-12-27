2b644885b0492bc53bea0f142dd98f951a844729
tasuten <heliopause64@gmail.com>
2016-12-27 23:40:23 +0900

    Add error check to detect in-work-tree

--- cd-gitroot.fish
+++ cd-gitroot.fish
@@ -4,3 +4,13 @@ function __cdgitroot_usage
     echo 'If PATH is specified, it\'s recognized as relative path'
     echo 'from repository\'s root directory. And jump to it.'
 end
+
+function cd-gitroot -d 'Jump to git repo\'s root'
+    if not command git rev-parse --git-dir > /dev/null ^ /dev/null
+        echo 'It\'s out of working tree!' 1>&2
+        return 1
+    end
+
+
+end
+
