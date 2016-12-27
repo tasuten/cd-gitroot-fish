abab78b34483e32b57325b0dc2396dbbf9f305b2
tasuten <heliopause64@gmail.com>
2016-12-27 23:24:27 +0900

    Write usage

---
+++ cd-gitroot.fish
@@ -0,0 +1,6 @@
+function __cdgitroot_usage
+    echo 'Usage: cd-gitroot [PATH]'
+    echo 'Jump to git repository\'s root directory.'
+    echo 'If PATH is specified, it\'s recognized as relative path'
+    echo 'from repository\'s root directory. And jump to it.'
+end
